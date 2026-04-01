870b514572b8ae4ab6ecf8ca1c8b21f9b65cd937
gowrivijayan-coder <gowrivijikalai9@gmail.com>
2026-04-01 16:03:04 +0530

    Initial commit

--- controllers/empdetails.controllers.js
+++ controllers/empdetails.controllers.js
@@ -1,6 +1,12 @@
 import Empdetails from "../models/empdetails.model.js";
-export const empdetailread = (req, res) => {
-  res.send("reading the employee users");
+export const empdetailread = async (req, res) => {
+  try {
+    const reademp = await Empdetails.find();
+    res.json(reademp);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  //   res.send("reading the employee users");
 };
 export const empdetailscreate = async (req, res) => {
   //name,empid,email,jioning date,department,role
@@ -23,10 +29,52 @@ export const empdetailscreate = async (req, res) => {
   }
   // return res.json(req.body);
 };
-
-export const empdetailupdate = (req, res) => {
-  res.send("updating the employee users");
+export const empidchecking = async (req, res) => {
+  try {
+    const checked = await Empdetails.findById(req.params.id);
+    if (checked == null) {
+      return res.status(404).json({ message: "cannot find the employee" });
+    } else {
+      res.json(checked);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
-export const empdetailsdelete = (req, res) => {
-  res.send("deleting  the employee users");
+export const empdetailupdate = async (req, res) => {
+  try {
+    const empupdate = await Empdetails.findByIdAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        name: req.body.name,
+        email: req.body.email,
+      },
+      {
+        new : true 
+      },
+     
+    );
+     res.status(200).json(empupdate);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+  //  res.end("updating the employee users");
+};
+export const empdetailsdelete = async (req, res) => {
+  try {
+     const empdelete = await Empdetails.deleteOne(
+      {
+        _id: req.params.id,
+      }
+  
+  
+);
+res.status(200).json(empdelete);
+
+} catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+  // res.send("deleting  the employee users");
 };
--- routes/empdetails.route.js
+++ routes/empdetails.route.js
@@ -1,13 +1,20 @@
-import express from "express"
-import { empdetailread, empdetailscreate, empdetailsdelete, empdetailupdate } from "../controllers/empdetails.controllers.js";
-const router=express.Router();
+import express from "express";
+import {
+  empdetailread,
+  empdetailscreate,
+  empdetailsdelete,
+  empdetailupdate,
+  empidchecking,
+} from "../controllers/empdetails.controllers.js";
+const router = express.Router();
 // for creating the CRUD operations
 //R=reading the emp details
-router.get("/",empdetailread)
+router.get("/", empdetailread);
+router.get("/:id", empidchecking);
 //c=creating the emp details
-router.post("/",empdetailscreate)
+router.post("/", empdetailscreate);
 //U=updating  the emp details
-router.put("/:id",empdetailupdate)
+router.put("/:id", empdetailupdate);
 //D = for deleting the emp details
-router.delete("/",empdetailsdelete)
+router.delete("/:id", empdetailsdelete);
 export default router;
